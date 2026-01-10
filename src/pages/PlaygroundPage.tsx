b71fcff662ba7c2b51b9a9ebb0f9628a118ecc79
shuvam_776 <shuvamsatapathi@gmail.com>
2026-01-11 00:09:44 +0530

    Fixed issues

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,11 @@
+import Footer from '@/components/footer/Footer'
 import PlaygroundContent from '@/components/ui/playground'
 
 export default function Playground() {
   return (
     <main className='w-full overflow-x-hidden'>
       <PlaygroundContent />
+      <Footer />
     </main>
   )
 }
