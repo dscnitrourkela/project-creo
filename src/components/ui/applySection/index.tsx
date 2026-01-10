95a7de83528722382c98173bdafd1fd8438915f0
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-01-10 12:43:23 +0530

    fix: imgs link added to separate folder in assets

--- src/components/ui/applySection/index.tsx
+++ src/components/ui/applySection/index.tsx
@@ -1,22 +1,18 @@
+import imgs from '@/assets/imgs/ApplySection'
+
 function ApplySection() {
   const handleEarlyRegistration = async () => {}
 
   const handleRegularRegistration = async () => {}
 
   return (
-    <div className="min-h-screen bg-[url('https://ik.imagekit.io/euurqxbuc/uploaded-asset_vYBs-degh')] bg-center bg-no-repeat bg-cover bg-black flex flex-col items-center min-w-scren overflow-x-hidden">
+    <div
+      className={`min-h-screen bg-[url('${imgs.bg}')] bg-center bg-no-repeat bg-cover bg-black flex flex-col items-center min-w-scren overflow-x-hidden`}
+    >
       <h1 className='scroll-m-20 text-4xl tracking-tight lg:text-4xl font-fort text-white flex items-center gap-3 [@media(max-width:530px)]:text-[1.3rem]'>
-        <img
-          src='https://ik.imagekit.io/euurqxbuc/uploaded-asset_VnDHU6kQr'
-          alt=''
-          className='w-18 [@media(max-width:530px)]:w-12'
-        />
+        <img src={imgs.leftBolt} alt='' className='w-18 [@media(max-width:530px)]:w-12' />
         <p>Apply Right Away!</p>
-        <img
-          src='https://ik.imagekit.io/euurqxbuc/uploaded-asset_WhfzGlVzm2'
-          alt=''
-          className='w-18 [@media(max-width:530px)]:w-12'
-        />
+        <img src={imgs.rightBolt} alt='' className='w-18 [@media(max-width:530px)]:w-12' />
       </h1>
       <h2
         className='text-neutral-200 scroll-m-20 text-md tracking-tight font-gabarito -mt-1
@@ -26,7 +22,7 @@ function ApplySection() {
       </h2>
       <div className='flex w-full overflow-visible mt-40 [@media(max-width:703px)]:mt-10 items-center justify-center mx-5 px-5 mb-5 relative'>
         <img
-          src='https://ik.imagekit.io/euurqxbuc/uploaded-asset_k_5DRK8sW'
+          src={imgs.blurCube}
           alt=''
           className='absolute left-1/2 -translate-x-[calc(50%+20.9rem)] top-1/2 -translate-y-1/5 w-56 z-10'
         />
@@ -73,7 +69,7 @@ function ApplySection() {
           </div>
         </div>
         <img
-          src='https://ik.imagekit.io/euurqxbuc/uploaded-asset_DL1c9u-Qk'
+          src={imgs.normalCube}
           alt=''
           className=' absolute left-1/2 translate-x-[calc(50%+7rem)] top-1/8 -translate-y-1/2 w-56 z-10'
         />
